a207133324a1417a2626cfda641010ae51ef5701
Himanshu Pandey <himanshu21pandey@gmail.com>
2023-06-24 12:38:50 +0530

    Setup backtest modules

--- src/strategy/trend/trend.controller.ts
+++ src/strategy/trend/trend.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { TrendService } from './trend.service';
 
 @Controller('trend')
-export class TrendController {}
+export class TrendController {
+  constructor(private readonly trendService: TrendService) {}
+}
--- src/strategy/trend/trend.module.ts
+++ src/strategy/trend/trend.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OHLC } from 'src/entities/ohlc.entity';
+import { Stock } from 'src/entities/stocks.entity';
 import { TrendController } from './trend.controller';
 import { TrendService } from './trend.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Stock, OHLC])],
   controllers: [TrendController],
-  providers: [TrendService]
+  providers: [TrendService],
 })
 export class TrendModule {}
